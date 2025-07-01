576a687ebd1aed3463cd2f48e7ada2c57b7ddd8d
Pontinn <leo.pontin2@gmail.com>
2025-07-01 09:24:50 -0300

    feat: create ChatInput and ChatOutPut

---
+++ src/main/java/dev/pontin/livechat/domain/ChatInput.java
@@ -0,0 +1,4 @@
+package dev.pontin.livechat.domain;
+
+public record ChatInput(String user, String message) {
+}
---
+++ src/main/java/dev/pontin/livechat/domain/ChatOutput.java
@@ -0,0 +1,4 @@
+package dev.pontin.livechat.domain;
+
+public record ChatOutput(String content) {
+}
